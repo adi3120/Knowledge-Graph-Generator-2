d3ca37492f43d367c71787657910a943d1d6f507
Aditya yadav <83342917+adi3120@users.noreply.github.com>
2023-09-15 20:23:44 +0530

    Update AssemblyAIHandler.py

--- AssemblyAIHandler.py
+++ AssemblyAIHandler.py
@@ -3,7 +3,7 @@ from tempfile import NamedTemporaryFile
 import streamlit as st
 
 class AssemblyHandler:
-	def __init__(self,api_key=f"3539fcd5f0c841779e6197159dfc2bdb"):
+	def __init__(self,api_key=st.secrets["ASSEMBLYAI_API_KEY"]):
 		aai.settings.api_key = api_key
 	def get_audio_transcript(self,linkaudio):
 		
